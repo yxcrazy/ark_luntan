e1ddbcf552f77aa827a24c2accb74b28d3f97b93
Yuanxin <466028676@qq.com>
2018-09-09 11:46:37 +0800

    添加了服务器状态

--- forum.php
+++ forum.php
@@ -17,39 +17,42 @@ require './source/class/class_core.php';
 
 require './source/function/function_forum.php';
 
+require './source/function/function_ark_server.php';
 
-$modarray = array('ajax','announcement','attachment','forumdisplay',
-	'group','image','index','medal','misc','modcp','notice','post','redirect',
-	'relatekw','relatethread','rss','topicadmin','trade','viewthread','tag','collection','guide'
+$modarray = array('ajax', 'announcement', 'attachment', 'forumdisplay',
+    'group', 'image', 'index', 'medal', 'misc', 'modcp', 'notice', 'post', 'redirect',
+    'relatekw', 'relatethread', 'rss', 'topicadmin', 'trade', 'viewthread', 'tag', 'collection', 'guide'
 );
 
 $modcachelist = array(
-	'index'		=> array('announcements', 'onlinelist', 'forumlinks',
-			'heats', 'historyposts', 'onlinerecord', 'userstats', 'diytemplatenameforum'),
-	'forumdisplay'	=> array('smilies', 'announcements_forum', 'globalstick', 'forums',
-			'onlinelist', 'forumstick', 'threadtable_info', 'threadtableids', 'stamps', 'diytemplatenameforum'),
-	'viewthread'	=> array('smilies', 'smileytypes', 'forums', 'usergroups',
-			'stamps', 'bbcodes', 'smilies',	'custominfo', 'groupicon', 'stamps',
-			'threadtableids', 'threadtable_info', 'posttable_info', 'diytemplatenameforum'),
-	'redirect'	=> array('threadtableids', 'threadtable_info', 'posttable_info'),
-	'post'		=> array('bbcodes_display', 'bbcodes', 'smileycodes', 'smilies', 'smileytypes',
-			'domainwhitelist', 'albumcategory'),
-	'space'		=> array('fields_required', 'fields_optional', 'custominfo'),
-	'group'		=> array('grouptype', 'diytemplatenamegroup'),
+    'index' => array('announcements', 'onlinelist', 'forumlinks',
+        'heats', 'historyposts', 'onlinerecord', 'userstats', 'diytemplatenameforum'),
+    'forumdisplay' => array('smilies', 'announcements_forum', 'globalstick', 'forums',
+        'onlinelist', 'forumstick', 'threadtable_info', 'threadtableids', 'stamps', 'diytemplatenameforum'),
+    'viewthread' => array('smilies', 'smileytypes', 'forums', 'usergroups',
+        'stamps', 'bbcodes', 'smilies', 'custominfo', 'groupicon', 'stamps',
+        'threadtableids', 'threadtable_info', 'posttable_info', 'diytemplatenameforum'),
+    'redirect' => array('threadtableids', 'threadtable_info', 'posttable_info'),
+    'post' => array('bbcodes_display', 'bbcodes', 'smileycodes', 'smilies', 'smileytypes',
+        'domainwhitelist', 'albumcategory'),
+    'space' => array('fields_required', 'fields_optional', 'custominfo'),
+    'group' => array('grouptype', 'diytemplatenamegroup'),
 );
+
+$ark = getserverstatus();
 
 $mod = !in_array(C::app()->var['mod'], $modarray) ? 'index' : C::app()->var['mod'];
 
 define('CURMODULE', $mod);
 $cachelist = array();
-if(isset($modcachelist[CURMODULE])) {
-	$cachelist = $modcachelist[CURMODULE];
+if (isset($modcachelist[CURMODULE])) {
+    $cachelist = $modcachelist[CURMODULE];
 
-	$cachelist[] = 'plugin';
-	$cachelist[] = 'pluginlanguage_system';
+    $cachelist[] = 'plugin';
+    $cachelist[] = 'pluginlanguage_system';
 }
-if(C::app()->var['mod'] == 'group') {
-	$_G['basescript'] = 'group';
+if (C::app()->var['mod'] == 'group') {
+    $_G['basescript'] = 'group';
 }
 
 C::app()->cachelist = $cachelist;
@@ -65,9 +68,8 @@ set_rssauth();
 runhooks();
 
 
-
 $navtitle = str_replace('{bbname}', $_G['setting']['bbname'], $_G['setting']['seotitle']['forum']);
 $_G['setting']['threadhidethreshold'] = 1;
-require DISCUZ_ROOT.'./source/module/forum/forum_'.$mod.'.php';
+require DISCUZ_ROOT . './source/module/forum/forum_' . $mod . '.php';
 
 ?>
---
+++ source/function/function_ark_server.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: function_attachment.php 28348 2012-02-28 06:16:29Z monkey $
+ */
+
+if (!defined('IN_DISCUZ')) {
+    exit('Access Denied');
+}
+
+function getserverstatus()
+{
+    $url = 'http://arkdedicated.com/news.ini';
+    return curl_request($url);
+}
+
+//参数1：访问的URL，参数2：post数据(不填则为GET)，参数3：提交的$cookies,参数4：是否返回$cookies
+function curl_request($url, $post = '', $cookie = '', $returnCookie = 0)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+    curl_setopt($curl, CURLOPT_REFERER, "http://XXX");
+    if ($post) {
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+    }
+    if ($cookie) {
+        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+    }
+    curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($curl);
+    if (curl_errno($curl)) {
+        return curl_error($curl);
+    }
+    curl_close($curl);
+    if ($returnCookie) {
+        list($header, $body) = explode("\r\n\r\n", $data, 2);
+        preg_match_all("/Set\-Cookie:([^;]*);/", $header, $matches);
+        $info['cookie'] = substr($matches[1][0], 1);
+        $info['content'] = $body;
+        return $info;
+    } else {
+        return $data;
+    }
+}
+
+?>
